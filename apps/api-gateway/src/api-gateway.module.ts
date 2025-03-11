53ed7c7c18edaf62cb429744eb7ec955a9142201
Hoanghaaii <haivu04112003@gmail.com>
2025-03-11 22:59:23 +0700

    first communicate api gateway

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { ApiGatewayController } from './api-gateway.controller';
 import { ApiGatewayService } from './api-gateway.service';
 import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ApiGatewayController } from './api-gateway.controller';
 
 @Module({
   imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'gateway-consumer-group', // Đảm bảo groupId khác với user-service nếu cần
+          },
+        },
+      },
+    ]),
     ConfigModule.forRoot({
       envFilePath: 'api-gateway/.env',
     })
--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { ApiGatewayModule } from './api-gateway.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApiGatewayModule);
-  await app.listen(process.env.port ?? 3000);
-  console.log(`API Gateway is running on: ${await app.getUrl()}`);
+  const app = await NestFactory.createMicroservice(ApiGatewayModule, {
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'], // Đảm bảo khớp với Kafka broker của bạn
+      },
+      consumer: {
+        groupId: 'gateway-consumer-group', // Consumer group cho api-gateway
+      },
+    },
+  });
+
+  await app.listen();
+  console.log('API Gateway is running as Kafka microservice');
 }
 bootstrap();
